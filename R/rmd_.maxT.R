7ce7511f79b3d489eef1d8533a35c7240cb7c41a
Tingting Zhan <tingtingzhan@gmail.com>
2025-03-16 15:58:13 -0400

    test

--- R/rmd_.maxT.R
+++ R/rmd_.maxT.R
@@ -14,10 +14,13 @@
 #' @keywords internal
 #' @export
 rmd_.maxT <- function(x, xnm, ...) c(
-  '```{r results = \'asis\'}', 
+  
+  # ?rmarkdown.tzh::rmd_.reactable ready
+  '```{r}', 
   sprintf(fmt = 'reactable_maxT(%s)', xnm),
   '```', 
-  sprintf(fmt = '```{r results = \'asis\', fig.height = %.1f, fig.width = %.1f}', 7, 6), 
+  
+  sprintf(fmt = '```{r fig.height = %.1f, fig.width = %.1f}', 7, 6), 
   sprintf(fmt = 'suppressWarnings(grid::grid.draw(%s@gtable))', xnm), 
   # ?grid:::grid.draw.gTree; 
   # workhorse ?grid:::drawGTree
