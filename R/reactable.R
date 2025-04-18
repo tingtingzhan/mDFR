907720b256774c64026b661e91180c9c6dec9257
Tingting Zhan <tingtingzhan@gmail.com>
2025-04-17 23:02:21 -0400

    tzh

--- R/reactable.R
+++ R/reactable.R
@@ -13,13 +13,13 @@
 #' @returns
 #' Function [reactable_maxT] returns a \link[reactable]{reactable} object.
 #' 
-#' @importFrom flextable.tzh format_pval
+#' @importFrom scales.tzh label_pvalue_sym
 #' @importFrom reactable reactable
 #' @export
 reactable_maxT <- function(x, ...) {
   
   z <- as.data.frame.maxT(x)
-  z$adjp <- format_pval(z$adjp)
+  z$adjp <- z$adjp |> label_pvalue_sym()()
   z$tstat <- round(z$tstat, digits = 3L)
   if (length(z[['abs(tstat)']])) z[['abs(tstat)']] <- round(z[['abs(tstat)']], digits = 3L)
   reactable(z, ...)
