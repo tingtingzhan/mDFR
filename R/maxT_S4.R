644369aca8f016148f196f09a01b8f17d04deee5
Tingting Zhan <tingtingzhan@gmail.com>
2026-03-11 17:25:33 -0400

    tzh

--- R/maxT_S4.R
+++ R/maxT_S4.R
@@ -166,26 +166,7 @@ as.data.frame.maxT <- function(
 
 
 
-#' @title Create \link[reactable]{reactable} from \linkS4class{maxT} Object
-#' 
-#' @description
-#' To create a \link[reactable]{reactable} from a \linkS4class{maxT} object.
-#' 
-#' @param x a \linkS4class{maxT} object
-#' 
-#' @param ... additional parameters of function \link[reactable]{reactable}
-#' 
-#' @returns
-#' The function [reactable_maxT()] returns a \link[reactable]{reactable} object.
-#' 
-#' @keywords internal
-#' @importFrom reactable reactable
-#' @export
-reactable_maxT <- function(x, ...) {
-  x |>
-    as.data.frame.maxT() |>
-    reactable(...)
-}
+
 
 
 
@@ -194,8 +175,8 @@ reactable_maxT <- function(x, ...) {
 
 setMethod(f = show, signature = 'maxT', definition = function(object) {
   object |>
-    reactable_maxT() |>
-    print() # ?htmlwidgets:::print.htmlwidget
+    as.data.frame.maxT() |>
+    print()
 })
 
 
